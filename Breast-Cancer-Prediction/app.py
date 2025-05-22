5df9897c1132b2297a69b4d199d69da7a76117ce
Chizycodes <okwudirejoy@gmail.com>
2025-05-22 14:20:12 +0100

    chatscholar app

--- Breast-Cancer-Prediction/app.py
+++ Breast-Cancer-Prediction/app.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
